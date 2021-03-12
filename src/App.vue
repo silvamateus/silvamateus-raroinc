a20db9d9df7093236dfb493a949f5b94c11cf481
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2021-03-12 17:15:02 -0300

    add TopHeader

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <top-header />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import TopHeader from "./components/TopHeader.vue";
 export default {
   name: "App",
-  components: {
-    HelloWorld
-  }
+  components: { TopHeader }
 };
 </script>
 
 <style>
+* {
+  font-family: "Roboto", sans-serif;
+  box-sizing: border-box;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #323469;
 }
 </style>

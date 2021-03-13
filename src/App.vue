de48302678b6f07ffdd086fb2a4a92759b351266
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2021-03-13 10:38:29 -0300

    add Blog component

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,16 @@
     <div class="header-background">
       <top-header />
     </div>
+    <blog />
   </div>
 </template>
 
 <script>
+import Blog from "./components/Blog.vue";
 import TopHeader from "./components/TopHeader.vue";
 export default {
   name: "App",
-  components: { TopHeader }
+  components: { TopHeader, Blog }
 };
 </script>
 
@@ -28,7 +30,13 @@ export default {
   color: #323469;
 }
 
-.header-background {
+.header-background::before {
+  content: " ";
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
   background: url("https://i.picsum.photos/id/46/650/650.jpg?hmac=DUt9PViAbahIOKRB5yvSnnw2YHVT4-wtybHv504sRmM");
   background-color: rgba(255, 255, 255, 0.37);
   background-repeat: no-repeat;
---
+++ src/components/Blog.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="blog">
+    <h2>Blog</h2>
+  </main>
+</template>
+
+<script>
+import posts from "@/services/posts";
+export default {
+  data() {
+    return { posts: [] };
+  },
+  created() {
+    posts()
+      .then(res => res.json())
+      .then(res => (this.posts = res))
+      .catch(err => console.log(err));
+  }
+};
+</script>
+
+<style></style>
